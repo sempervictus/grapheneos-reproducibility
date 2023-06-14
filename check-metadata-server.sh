a890805514ab67d0306fbf84372b68e8a435e9a9
Nullnet Services Administrator <admin@nullnet.services>
2023-06-13 19:29:54 -0500

    fix systemd service and script and compose.yml

--- check-metadata-server.sh
+++ check-metadata-server.sh
@@ -1,6 +1,24 @@
-[Unit]
-Description=Check Metadata Server for Update
+#!/bin/bash
 
-[Service]
-Type=simple
-ExecStart=/opt/check-metadata-server/check-metadata-server.sh
+DEVICE=$1
+CHANNEL=$2
+
+CURRENT_METADATA_FILE="/opt/grapheneos-update-metadata-${DEVICE}-${CHANNEL}"
+URL="https://releases.grapheneos.org/${DEVICE}-${CHANNEL}"
+
+if [ -f "$CURRENT_METADATA_FILE" ]; then
+    read -r CURRENT_METADATA_NUMBER CURRENT_METADATA_DATETIME CURRENT_METADATA_ID < $CURRENT_METADATA_FILE
+fi
+
+read -r BUILD_NUMBER BUILD_DATETIME BUILD_ID BRANCH < <(echo $(curl -s $URL))
+
+if [ -z "$CURRENT_METADATA_NUMBER" ] || [ "$BUILD_DATETIME" != "$CURRENT_METADATA_DATETIME" ]; then
+    # docker run --privileged 
+    echo "New version, updating..."
+elif [ "$BUILD_DATETIME" = "$CURRENT_METADATA_DATETIME" ]; then
+    echo "Same version, skipping..."
+else
+    echo "Something has gone seriously wrong. Hopefully the next one will correct it."
+fi
+
+echo "$BUILD_NUMBER" "$BUILD_DATETIME" "$BUILD_ID" "$BRANCH" > "$CURRENT_METADATA_FILE"
